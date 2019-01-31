3b9e9182da24c5be6e249cb60e4679d35762f950
Slava <lozovoyv@gmail.com>
2019-01-31 15:21:36 +0300

    Add tests

---
+++ tests/LogManagerTest.php
@@ -0,0 +1,145 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class LogManagerTest extends TestCase
+{
+    protected $config = [
+        'default' => 'testing',
+        'drivers' => [
+            'testing' => [
+                'driver' => TestingLogger::class,
+            ],
+        ],
+    ];
+
+    public static function setUpBeforeClass()
+    {
+        require __DIR__ . '/TestingLogger.php';
+        require __DIR__ . '/WrongTestingLogger.php';
+    }
+
+    public function test_No_Config(): void
+    {
+        $logger = new \OpxCore\Log\LogManager([]);
+
+        $this->expectException(\OpxCore\Log\Exceptions\LogManagerException::class);
+
+        $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
+    }
+
+    public function test_No_Driver(): void
+    {
+        $logger = new \OpxCore\Log\LogManager(['default' => 'testing', 'drivers' => []]);
+
+        $this->expectException(\OpxCore\Log\Exceptions\LogManagerException::class);
+
+        $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
+
+    }
+
+    public function test_Normal(): void
+    {
+        $logger = new \OpxCore\Log\LogManager([
+            'default' => 'testing',
+            'drivers' => [
+                'testing' => [
+                    'driver' => TestingLogger::class,
+                    'param' => 'Testing'
+                ],
+            ],
+        ]);
+
+        $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
+
+        $driver = $logger->driver();
+
+        $this->assertEquals([[
+            'level' => Psr\Log\LogLevel::DEBUG,
+            'message' => 'Test',
+            'context' => [],
+        ]], $driver->logs);
+    }
+
+    public function test_Wrong_Logger(): void
+    {
+        $logger = new \OpxCore\Log\LogManager([
+            'default' => 'testing',
+            'drivers' => [
+                'testing' => [
+                    'driver' => WrongTestingLogger::class,
+                    'param' => 'Testing'
+                ],
+            ],
+        ]);
+
+        $this->expectException(\OpxCore\Log\Exceptions\LogManagerException::class);
+
+        $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
+    }
+
+    public function test_Normal_Wrong_Level(): void
+    {
+        $logger = new \OpxCore\Log\LogManager([
+            'default' => 'testing',
+            'drivers' => [
+                'testing' => [
+                    'driver' => TestingLogger::class,
+                ],
+            ],
+        ]);
+
+        $this->expectException(\Psr\Log\InvalidArgumentException::class);
+
+        $logger->log('wrong', 'Test');
+    }
+
+    public function test_Driver_Not_Found(): void
+    {
+        $logger = new \OpxCore\Log\LogManager([
+            'default' => 'testing',
+            'drivers' => [
+                'testing' => [
+                    'driver' => NotTestingLogger::class,
+                ],
+            ],
+        ]);
+
+        $this->expectException(\OpxCore\Log\Exceptions\LogManagerException::class);
+
+        $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
+    }
+
+    public function test_Driver_No_Param(): void
+    {
+        $logger = new \OpxCore\Log\LogManager([
+            'default' => 'testing',
+            'drivers' => [
+                'testing' => [
+                    'driver' => TestingLogger::class,
+                ],
+            ],
+        ]);
+
+        $this->expectException(\OpxCore\Log\Exceptions\LogManagerException::class);
+
+        $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
+    }
+
+    public function test_No_Driver_Class(): void
+    {
+        $logger = new \OpxCore\Log\LogManager([
+            'default' => 'testing',
+            'drivers' => [
+                'testing' => [
+                    'driver' => '',
+                    'param' => 'Testing'
+                ],
+            ],
+        ]);
+
+        $this->expectException(\OpxCore\Log\Exceptions\LogManagerException::class);
+
+        $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
+    }
+}
---
+++ tests/TestingLogger.php
@@ -0,0 +1,31 @@
+<?php
+
+class TestingLogger extends \Psr\Log\AbstractLogger
+{
+    public $logs = [];
+
+    public $param;
+
+    public function __construct($param)
+    {
+        $this->param = $param;
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function log($level, $message, array $context = array()): void
+    {
+        $this->logs[] = [
+            'level' => $level,
+            'message' => $message,
+            'context' => $context,
+        ];
+    }
+}
