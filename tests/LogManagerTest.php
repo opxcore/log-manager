f427b446de374a061b154fbdd4e2d97a6a13eec6
Slava <lozovoyv@gmail.com>
2019-02-02 15:51:31 +0300

    Update tests

--- tests/LogManagerTest.php
+++ tests/LogManagerTest.php
@@ -43,81 +43,97 @@ class LogManagerTest extends TestCase
         $logger = new \OpxCore\Log\LogManager([
             'default' => 'testing',
             'loggers' => [
-                'testing' => [
+                'testing1' => [
                     'driver' => TestingLogger::class,
                     'param' => 'Testing'
                 ],
             ],
         ]);
 
-        $driver = new TestingLogger('testing');
+        $driver1 = new TestingLogger('testing 1');
+        $driver2 = new TestingLogger('testing 2');
 
-        $logger->registerLogger('testing', function () use ($driver) {
-            return $driver;
+        $logger->registerLogger('testing', function () use ($driver1) {
+            return $driver1;
+        });
+
+        $logger->registerLogger('testing2', function () use ($driver2) {
+            return $driver2;
         });
 
         $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::DEBUG,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
 
         $logger->emergency('Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::EMERGENCY,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
 
         $logger->alert('Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::ALERT,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
 
         $logger->critical('Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::CRITICAL,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
 
         $logger->error('Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::ERROR,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
 
         $logger->warning('Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::WARNING,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
 
         $logger->notice('Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::NOTICE,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
 
         $logger->info('Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::INFO,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
 
         $logger->debug('Test');
         $this->assertEquals(
             [['level' => Psr\Log\LogLevel::DEBUG,'message' => 'Test', 'context' => []]],
-            $driver->logs
+            $driver1->logs
         );
-        $driver->logs = [];
+        $driver1->logs = [];
+        $driver2->logs = [];
+
+        $logger->driver(['testing1','testing2','testing'])->debug('Test');
+        $this->assertEquals(
+            [['level' => Psr\Log\LogLevel::DEBUG,'message' => 'Test', 'context' => []]],
+            $driver1->logs
+        );
+        $this->assertEquals(
+            [['level' => Psr\Log\LogLevel::DEBUG,'message' => 'Test', 'context' => []]],
+            $driver2->logs
+        );
     }
 
     public function test_Wrong_Logger(): void
@@ -135,22 +151,6 @@ class LogManagerTest extends TestCase
         $this->expectException(\OpxCore\Log\Exceptions\LogManagerException::class);
 
         $logger->log(Psr\Log\LogLevel::DEBUG, 'Test');
-    }
-
-    public function test_Normal_Wrong_Level(): void
-    {
-        $logger = new \OpxCore\Log\LogManager([
-            'default' => 'testing',
-            'loggers' => [
-                'testing' => [
-                    'driver' => TestingLogger::class,
-                ],
-            ],
-        ]);
-
-        $this->expectException(\Psr\Log\InvalidArgumentException::class);
-
-        $logger->log('wrong', 'Test');
     }
 
     public function test_Driver_Not_Found(): void
